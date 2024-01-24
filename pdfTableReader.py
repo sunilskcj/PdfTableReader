d064a6dcf7f90be5ad80aa2ebf1d5dd6b7910fab
sunilskcj <sunilskcj@gmail.com>
2024-01-24 12:54:06 +0530

    constructor

--- pdfTableReader.py
+++ pdfTableReader.py
@@ -7,7 +7,13 @@ import requests
 
 import pdfplumber
 class TransactionModel:
-
+    # def __init__(self, date, name):
+    #     self.date = date
+    #     self.age = name
+    # def custom_serializer(obj):
+    #     if isinstance(obj, TransactionModel):
+    #         return {"name": obj.date, "age": obj.name}
+    #     raise TypeError("Object is not JSON serializable.")
     TranListOfPages=[]
     with pdfplumber.open("XXXX",password= 'XXX') as f:
         for i in f.pages:
